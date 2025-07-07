719a1cfb541d1c8084773e64a9bee649351223e0
EbaadAwan <ebaadawan213@gmail.com>
2025-07-07 01:49:55 -0400

    Commit includes seventh problem set

---
+++ 238_Product_of_Array_Except_Self/solution.java
@@ -0,0 +1,22 @@
+// Ebaad Awan
+// July 7, 2025
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int [nums.length];
+        int product = 1;
+
+        for(int i = 0; i < nums.length; i++){
+            answer[i] = product;
+            product *= nums[i];
+        }
+
+        product = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            answer[i] *= product;
+            product *= nums[i];
+        }
+
+        return answer;
+    }
+}
